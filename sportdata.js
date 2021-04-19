3bf9ae0659edae2ac64c48a4731355a2c525a700
chiragpatel500 <chirag12354@gmail.com>
2021-04-19 17:02:44 +0200

    update

--- sportdata.js
+++ sportdata.js
@@ -1,13 +1,13 @@
 const fetchMyData = () => {
-  // Trying using authentication
-  const url = "https://www.thesportsdb.com/api/v1/json/1/all_sports.php";
-  fetch(url, {
-  method: "GET",
-  headers: {
-    "X-Auth-Token": "1",
-    "Content-Type": "application/json"
-  }
-  })
+  // // Trying using authentication
+  // const url = "https://www.thesportsdb.com/api/v1/json/1/all_sports.php";
+  // fetch(url, {
+  // method: "GET",
+  // headers: {
+  //   "X-Auth-Token": "1",
+  //   "Content-Type": "application/json"
+  // }
+  // })
 
     // Trying without authentication
   // step 0: make an api call using fetch.
@@ -15,16 +15,24 @@ const fetchMyData = () => {
   // fetch("https://cab-cors-anywhere.herokuapp.com/https://www.thesportsdb.com/api/v1/json/1/all_sports.php")
    // step 1: displaying respnse coming from the api  
    // using .then to catch the promise response coming from api and wraping it in to a function.  
-    .then((response) => {
+   const url = "https://cab-cors-anywhere.herokuapp.com/https://www.thesportsdb.com/api/v1/json/1/all_sports.php";
+  fetch(url, {
+  method: "GET",
+  headers: {
+    "X-Auth-Token": "1",
+    "Content-Type": "application/json"
+  }
+  }).then((response) => {
       // returning the response into json file to make it readable for the browser.
       return response.json();
     })
     //  step 2: display this step until step 1 is fully completed.
     // extracting the data and displaying loading until the data is displayed on the page.
-    .then((data) => {
+    .then((all_Sports) => {
       document.getElementById("loading").style.display = "none";
       // calling the displayData to perform inside the block.
-      displayData(data);
+      console.log(all_Sports.sports);
+      displayData(all_Sports.sports);
     })
     // step 3: display error message if any prbloem in step 1 for extracting the data from api.
     // cathing any error coming from the data or url.
@@ -45,14 +53,44 @@ const showError = (err) => {
 };
 
 // displaying data on the html page. 
-const displayData = (data) => {
+// const displayData = (sports) => {
+//   // selecting the elemnet from html page
+//   const ul = document.getElementById("list");
+//   // looping through the sports array.
+//   sports.forEach((sport) => {
+//     // creating a new element inside the html page
+//     const li = document.createElement("li");
+//     // assiging a values to the newly created element.
+//     li.innerHTML = sport.strSport;
+//     // appenind newly created elemnt to to an existing element as a child.
+//     ul.appendChild(li);
+//   });
+// }; 
+
+
+const displayData = (sports) => {
   // selecting the elemnet from html page
-  const ul = document.getElementById("list");
+  const tabData = document.getElementById("tablebody");
   // looping through the sports array.
-  data.forEach((all_sports) => {
-    const li = document.createElement("li");
-    li.innerHTML = all_sports.sports;
-    ul.appendChild(li);
+  sports.forEach((sport) => {
+    // creating a new element inside the html page
+    const tabTr = document.createElement("tr");
+    const tabtd1 = document.createElement("td");
+    const tabtd2 = document.createElement("td");
+    const tabtd3 = document.createElement("td");
+    const tabtd4 = document.createElement("td");
+
+    tabtd1.innerHTML = sport.strSport;
+    tabtd2.innerHTML = sport.strFormat;
+    tabtd3.innerHTML = sport.strSportThumb;
+    tabtd4.innerHTML = sport.strSportDescription;
+
+    tabTr.appendChild(tabtd1);
+    tabTr.appendChild(tabtd2);
+    tabTr.appendChild(tabtd3);
+    tabTr.appendChild(tabtd4);
+    
+    tabData.appendChild(tabTr);
   });
 }; 
 
@@ -60,9 +98,3 @@ const displayData = (data) => {
 
 
 
-
-// https://jsonplaceholder.typicode.com/posts
-
-
-// q1. not displaying data even with get apikey.
-// q2. discuss checkbox  selction and what should the outcomes of filtering.

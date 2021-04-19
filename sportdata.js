3cd64854610fa1c1e00b97a54a3014cadae35459
chiragpatel500 <chirag12354@gmail.com>
2021-04-19 14:21:03 +0200

    update

--- sportdata.js
+++ sportdata.js
@@ -1,5 +1,15 @@
 const fetchMyData = () => {
-  fetch("https://cab-cors-anywhere.herokuapp.com/https://www.thesportsdb.com/api/v1/json/1/all_sports.php")
+  // Trying using authentication
+  const url = "https://www.thesportsdb.com/api/v1/json/1/all_sports.php";
+  fetch(url, {
+  method: "GET",
+  headers: {
+    "X-Auth-Token": "1",
+    "Content-Type": "application/json"
+  }
+  })
+    // Trying without authentication
+  // fetch("https://cab-cors-anywhere.herokuapp.com/https://www.thesportsdb.com/api/v1/json/1/all_sports.php")
     .then((response) => {
       return response.json();
     })
@@ -34,3 +44,7 @@ const displayData = (data) => {
 
 
 // https://jsonplaceholder.typicode.com/posts
+
+
+// q1. not displaying data even with get apikey.
+// q2. discuss checkbox  selction and what should the outcomes of filtering.

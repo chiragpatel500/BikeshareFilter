079ecf5cd9861289e50d04300f45ec3dfa435afe
chiragpatel500 <chirag12354@gmail.com>
2021-07-15 14:15:10 +0200

    Minor UPDATES

--- sportdata.js
+++ sportdata.js
@@ -1,23 +1,22 @@
 const fetchMyData = () => {
- 
-
-    // Trying without authentication
+  // Trying without authentication
   // step 0: make an api call using fetch.
-    // make a call to get promise response using the fetch keyword.
+  // make a call to get promise response using the fetch keyword.
   // fetch("https://cab-cors-anywhere.herokuapp.com/https://www.thesportsdb.com/api/v1/json/1/all_sports.php")
 
-
-  // Trying with authentication 
+  // Trying with authentication
   // step 1: displaying respnse coming from the api
-   // using .then to catch the promise response coming from api and wraping it in to a function.  
-   const url = "https://cab-cors-anywhere.herokuapp.com/https://www.thesportsdb.com/api/v1/json/1/all_sports.php";
+  // using .then to catch the promise response coming from api and wraping it in to a function.
+  const url =
+    "https://cab-cors-anywhere.herokuapp.com/https://www.thesportsdb.com/api/v1/json/1/all_sports.php";
   fetch(url, {
-  method: "GET",
-  headers: {
-    "X-Auth-Token": "1",
-    "Content-Type": "application/json"
-  }
-  }).then((response) => {
+    method: "GET",
+    headers: {
+      "X-Auth-Token": "1",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
       // returning the response into json file to make it readable for the browser.
       return response.json();
     })
@@ -44,12 +43,12 @@ if (document.title === "sportlist") {
   fetchMyData();
 }
 
-// displaying the error message (step 3) on the html page. 
+// displaying the error message (step 3) on the html page.
 const showError = (err) => {
   document.getElementById("error").innerHTML = err;
 };
 
-// displaying data on the html page. 
+// displaying data on the html page.
 // const displayData = (sports) => {
 //   // selecting the elemnet from html page
 //   const ul = document.getElementById("list");
@@ -62,37 +61,35 @@ const showError = (err) => {
 //     // appenind newly created elemnt to to an existing element as a child.
 //     ul.appendChild(li);
 //   });
-// }; 
-
+// };
 
 const displayData = (sports) => {
   // selecting the elemnet from html page
   const tabData = document.getElementById("tablebody");
   tablebody.innerHTML = "";
 
-
-// looping through the sports array.
-  sports.forEach((sport,index) => {
+  // looping through the sports array.
+  sports.forEach((sport, index) => {
     // creating a new element inside the html page
     const tabTr = document.createElement("tr");
     const tabtd1 = document.createElement("td");
     const tabtd2 = document.createElement("td");
     const tabtd3 = document.createElement("td");
     const tabtd4 = document.createElement("td");
-    
-// making image out of a link.
+
+    // making image out of a link.
     let myImg = new Image();
     myImg.src = sport.strSportThumb;
     tabtd3.appendChild(myImg);
 
-// show more show less text for the description. 
-  // splitting the  description  
-    let splittedDescription = sport.strSportDescription.split(".")    
+    // show more show less text for the description.
+    // splitting the  description
+    let splittedDescription = sport.strSportDescription.split(".");
     // let shortText = splittedDescription
-    let shortText = splittedDescription[0]
-    splittedDescription.shift()
+    let shortText = splittedDescription[0];
+    splittedDescription.shift();
     console.log(splittedDescription);
-    let longText = splittedDescription.join('')
+    let longText = splittedDescription.join("");
     console.log(longText);
 
     tabtd1.innerHTML = sport.strSport;
@@ -100,13 +97,13 @@ const displayData = (sports) => {
     // tabtd3.innerHTML = sport.strSportThumb;
     tabtd4.innerHTML = shortText;
 
-//show more show less
+    //show more show less
     // creating the <p> tag
     let ptag = document.createElement("p");
     // assining id to <p> tag
     ptag.setAttribute("id", "ptagId-" + index);
-    // // styling the <p> tag 
-    ptag.style = "display: none";    
+    // // styling the <p> tag
+    ptag.style = "display: none";
     // creating the <button> tag.
     let btn = document.createElement("button");
     // assiging id to <button> tag.
@@ -118,19 +115,17 @@ const displayData = (sports) => {
 
     // adding an event listener to btn <button>tag
     btn.addEventListener("click", function (event) {
-      showmorefunc(event)
-    })
-    
+      showmorefunc(event);
+    });
 
     tabtd4.appendChild(ptag);
     tabtd4.appendChild(btn);
- 
-   
+
     tabTr.appendChild(tabtd1);
     tabTr.appendChild(tabtd2);
     tabTr.appendChild(tabtd3);
     tabTr.appendChild(tabtd4);
-  
+
     tabData.appendChild(tabTr);
   });
 };
@@ -138,7 +133,7 @@ const displayData = (sports) => {
 const showmorefunc = (event) => {
   let btnId = event.target.id;
   let numberIndex = btnId.split("-")[1];
-  
+
   let ptag = document.getElementById("ptagId-" + numberIndex);
   // ptag.style.display = "block";
   if (ptag.style.display === "block") {
@@ -150,10 +145,9 @@ const showmorefunc = (event) => {
   }
   console.log(ptag);
 
-  // 
+  //
   console.log(event.target.id);
 };
-
 
 // STEP 1: adding event listners to the checboxes and dropdown.
 
@@ -163,7 +157,7 @@ const addEvents = (sports) => {
   let checkboxes = Array.from(
     document.querySelectorAll("input[type=checkbox]")
   );
-// looping through the array of data 
+  // looping through the array of data
   checkboxes.forEach((checkbox) => {
     // adding event listener to the check boxes
     checkbox.addEventListener("change", () => {
@@ -188,9 +182,9 @@ const createSelectOptions = (sports) => {
   // creating a new arrayay to remove duplicates.
   let cleanedSportNames = [];
   // looping through the array to remove the duplicates
-  sportNames.forEach((strSport,index) => {
+  sportNames.forEach((strSport, index) => {
     // using indexOf method to remove duplicates.
-    if (sportNames.indexOf(strSport) === index) (sportNames.push(strSport))
+    if (sportNames.indexOf(strSport) === index) sportNames.push(strSport);
     {
       // pushing the result to an array.
       cleanedSportNames.push(strSport);
@@ -198,7 +192,7 @@ const createSelectOptions = (sports) => {
   });
   console.log(cleanedSportNames);
 
-  // selecting the select dropdown and assiging an cleaned array value to it as well as changing the atribute. 
+  // selecting the select dropdown and assiging an cleaned array value to it as well as changing the atribute.
   let select = document.getElementById("selectDrop");
   cleanedSportNames.forEach((strSport) => {
     // creating a new element and rendering it dynamically.
@@ -209,12 +203,11 @@ const createSelectOptions = (sports) => {
   });
 };
 
-
 // writing the filter function with actually filters the data.
 const filterData = (sports) => {
-  console.log(sports)
+  console.log(sports);
   let checkboxes = Array.from(
-  // selecting the checkboxes and  mapping through entire object to revice the  checked value.
+    // selecting the checkboxes and  mapping through entire object to revice the  checked value.
     document.querySelectorAll("input[type=checkbox]:checked")
     // mapping through the chechboxes
   ).map((checkbox) => {
@@ -225,41 +218,43 @@ const filterData = (sports) => {
   let selectElm = document.getElementById("selectDrop").value;
   console.log(selectElm);
   console.log(checkboxes);
-  let filteredData = [];  
+  let filteredData = [];
   // 1 Conditition : if nothing slected than display all
   if (selectElm === "all" && checkboxes.length === 0) {
-    filteredData = sports
+    filteredData = sports;
     // 2 condition : if only checkbox is selected than only display data from checkdown.
   } else if (selectElm === "all" && checkboxes.length !== 0) {
-     sports.forEach((sport) => {
+    sports.forEach((sport) => {
       if (checkboxes.includes(sport.strFormat)) {
         filteredData.push(sport);
       }
-     });
-    // 3 condition : if only dropdown is selected than only display data from the Dropdown. 
+    });
+    // 3 condition : if only dropdown is selected than only display data from the Dropdown.
   } else if (selectElm !== "all" && checkboxes.length === 0) {
     sports.forEach((sport) => {
       if (selectElm === sport.strSport) {
         filteredData.push(sport);
       }
     });
-    // 4 condition: if both are selected than display data from all.  
+    // 4 condition: if both are selected than display data from all.
   } else {
-    sports.forEach((sport) => {      
-      if (selectElm === sport.strSport && checkboxes.includes(sport.strFormat)) {
+    sports.forEach((sport) => {
+      if (
+        selectElm === sport.strSport &&
+        checkboxes.includes(sport.strFormat)
+      ) {
         filteredData.push(sport);
       }
     });
   }
-  displayData(filteredData)
+  displayData(filteredData);
 };
-
 
 // 1 : show more common function
 // 2 :Html to html
-// 3 : media query 
+// 3 : media query
 // need to make another 2nd table for the mobile version media query.
 
 // 1st class desktop table: display :block
-// 2nd class mobile table : display : none 
+// 2nd class mobile table : display : none
 // using the media query invert the display property.
